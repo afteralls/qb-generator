7e0a3e6be1244741ec7753453340dc8ace498be8
Post-Apocalypse <postapocalypsecore@gmail.com>
2022-07-19 16:37:29 +0600

    just commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-page">
+    <h1>Генератор штрих-кодов</h1>
+    <p>Приложение для генерации штрих-кодов в различныйх форматах</p>
+    <button class="_btn" @click.prevent="$router.push('/generator')">Перейти к Генератору</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import '../assets/scss/variables';
+
+.main-page {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: $gap;
+}
+</style>
