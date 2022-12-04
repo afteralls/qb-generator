ab4eeedd59f50bfddd4f66a90d3edceb888a8962
Post-Apocalypse <postapocalypsecore@gmail.com>
2022-12-04 14:19:47 +0600

    add lang changes

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import i18n from './plugins/i18n.js'
+import { en, ru } from './languages/langData.js'
 
 import '@/assets/styles/main.scss'
 
 const app = createApp(App)
-app.use(createPinia()).use(router).mount('#app')
+app.use(createPinia()).use(router).use(i18n, { en, ru }).mount('#app')
