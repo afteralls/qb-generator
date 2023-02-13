e0f02063206d00fe8cd475158d2c2bec942bc6a9
Post-Apocalypse <postapocalypsecore@gmail.com>
2023-02-14 01:43:45 +0600

    hotfix

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <TheNavbar />
+  <AppHotifications />
+  <AppBackground class="app-background _full-fixed" />
+  <div class="app-wrapper _full-fixed"></div>
   <div class="_container">
     <RouterView v-slot="{ Component }">
       <Transition name="main" mode="out-in">
-        <component :is="Component" />
+        <div class="_wrapper">
+          <component :is="Component" />
+        </div>
       </Transition>
     </RouterView>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const router = useRouter()
-const i18n = inject('func')
-router.beforeEach(to => { document.title = i18n(to.name) })
+const i18n = inject('func') as LangFunc
+router.beforeEach((to, _, next) => { document.title = i18n(to.name as string); next() })
 </script>
+
+<style scoped lang="scss">
+.app-wrapper {
+  backdrop-filter: blur(40px);
+  background-color: var(--tp-c);
+  z-index: -1;
+}
+
+.app-background { z-index: -2; }
+</style>
